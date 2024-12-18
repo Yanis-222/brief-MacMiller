337b4adea16ac16de4b3959dc13a5f88ee41a059
Blues 🦋 <yanis.anciaux.dwwm48@gmail.com>
2024-12-18 09:54:33 +0100

    uptade yanis

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// Sélectionner le bouton et le body
+
 const toggleButton = document.getElementById('dark-mode-toggle');
 const body = document.body;
 
-// Vérifie le mode actuel enregistré dans localStorage
+// Vérifie le mode actuel 
 if (localStorage.getItem('theme') === 'light') {
     body.classList.add('light-theme');
     toggleButton.textContent = 'Mode Sombre';
 }
 
-// Fonction pour basculer le thème
+// changement du thème
 toggleButton.addEventListener('click', () => {
     if (body.classList.contains('light-theme')) {
         body.classList.remove('light-theme');
         localStorage.setItem('theme', 'dark');
-        toggleButton.textContent = 'Mode Sombre';
+        toggleButton.textContent = '🌙';
     } else {
         body.classList.add('light-theme');
         localStorage.setItem('theme', 'light');
-        toggleButton.textContent = 'Mode Clair';
+        toggleButton.textContent = '☀︎';
     }
 });
